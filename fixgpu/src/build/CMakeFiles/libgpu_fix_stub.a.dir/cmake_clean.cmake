0f19d3ba05cba45462e79a4f69d0f8a2652c8e2e
Your Name <you@example.com>
2021-12-21 18:15:06 +1300

    Fix crash screen

--- fixgpu/src/build/CMakeFiles/libgpu_fix_stub.a.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libgpu_fix_stub.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libgpu_fix_stub.a.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- fixgpu/src/build/CMakeFiles/libgpu_fix_stub_weak.a.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libgpu_fix_stub_weak.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libgpu_fix_stub_weak.a.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
